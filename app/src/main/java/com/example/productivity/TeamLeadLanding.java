8cd9e47d7597da401b5df80d7227208ed4f74f33
harshva89 <harshvaswani89@gmail.com>
2021-04-09 23:57:19 +0530

    created pages

---
+++ app/src/main/java/com/example/productivity/TeamLeadLanding.java
@@ -0,0 +1,14 @@
+package com.example.productivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamLeadLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_lead_landing);
+    }
+}
