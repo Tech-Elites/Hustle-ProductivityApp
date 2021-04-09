48aa4d38e3690e043a511cc3287876b75fc87208
sonashubhamvats <sona.svats23@gmail.com>
2021-04-10 01:25:50 +0530

    toolbar

--- app/src/main/java/com/example/productivity/EmployeeLanding.java
+++ app/src/main/java/com/example/productivity/EmployeeLanding.java
@@ -3,12 +3,29 @@ package com.example.productivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class EmployeeLanding extends AppCompatActivity {
 
+    TextView exclaimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_landing);
+        Toolbar t=findViewById(R.id.notificationToolBar);
+        setSupportActionBar(t);
+        exclaimation=findViewById(R.id.exclaimationwork);
+        exclaimation.setText("");
     }
+    public void OnClickNoti(View view)
+    {
+        Toast.makeText(this, "Hello notifications clicked", Toast.LENGTH_SHORT).show();
+        exclaimation.setText("!");
+    }
+
+
 }
